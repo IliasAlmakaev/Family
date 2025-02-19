0f14fab67978dfdedca3b380873c4ee8eff69e26
Ilias Almakaev <i.almakaev@gmail.com>
2025-02-19 22:32:29 +0700

    Add UI

---
+++ Family/FamilyViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FamilyViewController.swift
+//  Family
+//
+//  Created by Ilyas on 19.02.2025.
+//
+
+import UIKit
+
+class FamilyViewController: UIViewController {
+  
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var clearButton: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+  @IBAction func clearButtonPressed() {
+  }
+  
+}
+
